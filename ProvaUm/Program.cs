4f447f08cad10762ffd86f3c04eaee8d2c60b479
eduardokaique99 <eduardp_kaique99@hotmail.com>
2023-02-13 21:04:54 -0300

    Inicio

---
+++ ProvaUm/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
+using System.Text;
+
+namespace Prova1
+{
+    public class Prova 
+    {
+        public static void Main(String[] args) 
+        {
+            string[] lines = File.ReadAllLines("C:/Users/eduardo.furlaneto/OneDrive - SENAC-SC/DOTNET/Prova1/Prova1/naoordenado.txt", Encoding.UTF8);
+            Console.WriteLine("\nArray de entrada");
+            Array.ForEach(lines, item => { Console.WriteLine(item); });
+        }
+    }
+}
